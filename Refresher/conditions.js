5be47f292aefd0d6c955a77b781e63b7fae7faaf
hosseintaromi <hosseintaromii@gmail.com>
2024-08-08 08:33:21 +0330

    feat: :sparkles: add conditions examples

---
+++ Refresher/conditions.js
@@ -0,0 +1,97 @@
+if (condition) {
+  // code to be executed if condition is true
+}
+
+// if (condition) {
+//   // code to be executed if condition is true
+// } else {
+//   // code to be executed if condition is false
+// }
+
+// if (condition1) {
+//   // code to be executed if condition1 is true
+// } else if (condition2) {
+//   // code to be executed if condition2 is true
+// } else {
+//   // code to be executed if none of the conditions above are true
+// }
+
+// let result = condition ? value1 : value2;
+
+// if (condition1 && condition2) {
+//   // code to be executed if both condition1 and condition2 are true
+// }
+
+// if (condition1 || condition2) {
+//   // code to be executed if at least one of condition1 or condition2 is true
+// }
+
+// if (!condition) {
+//   // code to be executed if condition is false
+// }
+
+// if (a == b) {
+//   // code to be executed if a is equal to b
+// }
+
+// if (a === b) {
+//   // code to be executed if a is equal to and of the same type as b
+// }
+
+// if (a != b) {
+//   // code to be executed if a is not equal to b
+// }
+
+// if (a !== b) {
+//   // code to be executed if a is not equal to or not of the same type as b
+// }
+
+// if (a < b) {
+//   // code to be executed if a is less than b
+// }
+
+// if (a <= b) {
+//   // code to be executed if a is less than or equal to b
+// }
+
+// if (a > b) {
+//   // code to be executed if a is greater than b
+// }
+
+// if (a >= b) {
+//   // code to be executed if a is greater than or equal to b
+// }
+
+// condition && doSomething();
+// // doSomething() is executed if condition is true
+
+// condition || doSomething();
+// // doSomething() is executed if condition is false
+
+// let day = new Date().getDay(); // Get the current day of the week as a number (0-6)
+
+// switch (day) {
+//   case 0:
+//     console.log("Today is Sunday");
+//     break;
+//   case 1:
+//     console.log("Today is Monday");
+//     break;
+//   case 2:
+//     console.log("Today is Tuesday");
+//     break;
+//   case 3:
+//     console.log("Today is Wednesday");
+//     break;
+//   case 4:
+//     console.log("Today is Thursday");
+//     break;
+//   case 5:
+//     console.log("Today is Friday");
+//     break;
+//   case 6:
+//     console.log("Today is Saturday");
+//     break;
+//   default:
+//     console.log("Invalid day");
+// }
